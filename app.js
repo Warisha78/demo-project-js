efeedd682d8349f1d357fa288d41d78b12780e46
Warisha78 <warishaasif85@gmail.com>
2024-02-23 02:04:19 -0800

    initial commit

---
+++ app.js
@@ -0,0 +1,111 @@
+// Chapter1-3docx
+// Chapter1 Alert
+// Q1
+alert("First name");
+// ii
+
+alert("Lastname");
+// iii
+alert("Email");
+// iv
+alert("Phone number");
+// v
+alert("Password");
+// Q2
+alert("You're learning JavaScript!");
+// Q3
+alert("fill this Form Carefully!");
+
+// Chapter2 Variable for Strings
+// Q1
+// var iPhone
+// Q2
+var username = "Warisha";
+// Q3
+var teamname = "Team-X";
+// Q4
+var bestMan = "john doe";
+// Chapter3 Variable for Numbers
+// Q1,Q2
+var caseQty = "144";
+// Q3
+var num = "9";
+// Q4
+var num = 2;
+var num2 = 2;
+var total = num + num2;
+alert(num + num2);
+// Q5
+var merchtotal = 100;
+var shippingcharge = 10;
+var ordertotal = merchtotal + shippingcharge;
+console.log(ordertotal);
+
+// Q6
+var onebag = 100;
+var twobag = 10;
+var subtotal = onebag + twobag;
+console.log(subtotal);
+
+// Chapter ALert Pdf1
+alert("Hello World");
+// Q2
+alert("Please enter a valid password!");
+// Q3
+alert("Wlecome to JS land \n happy coding");
+
+// Q4
+alert("Wlecome to JS land ");
+
+alert("Prevent this page from creating additional dialogue ");
+// Q5
+console.log("Hello i can run JS through my browser's console");
+// Chapter Variable for Strings Pdf2
+// Q1
+var username;
+// Q2
+var myname = "Warisha";
+// Q3
+var messege = "hello world";
+alert(messege);
+// Q4
+var name = "John Doe";
+alert(name);
+var age = "14 years old";
+alert(age);
+var work = "Certified mobile application development";
+alert(work);
+// Q5
+var one = "PIZZA";
+var two = "PIZZ";
+var three = "PIZ";
+var four = "PI";
+var five = "P";
+alert(one + two + three + four + five);
+// Q6
+var email = "abc@gmail.com";
+alert("my email address is " + email);
+// Q7
+var book = "a smarter way to learn JS";
+alert("im trying to learn from " + book);
+// Q8
+document.wite = "yeah i can wrtie through JavaScript";
+// chpater 3 pdf
+// Q1
+var age = "i am 15 years old";
+alert(age);
+// Q2
+var remember = "you've visited this site twice";
+alert(remember);
+// Q3
+var birthyear = 1998;
+document.write(
+  "My birth year is " +
+    birthyear +
+    "\n Data type of my declared variable is number"
+);
+// Q4
+var visitorname = "john doe";
+var producttitle = "5 T shirts";
+var Quantity = "XYZ Clothing store";
+document.write(visitorname + "ordered" + producttitle + "on" + Quantity);
